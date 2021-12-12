d1a952f45497ce332701449c7762e9501720a10c
giornn0 <shaaamsuu@gmail.com>
2021-12-12 11:23:56 -0300

    fixed color,added blog section

--- components/article/Article.js
+++ components/article/Article.js
@@ -1,5 +1,22 @@
 import styles from './article.module.scss'
 
-export default function  Article(){
-    return <h1>Article</h1>
+import Image from 'next/image'
+
+export default function  Article({imgUrl,date,title}){
+    return (
+        <>
+        <div className={`${styles["gpt3__blog-container_article"]}`}>
+            <div className={`${styles["gpt3__blog-container_article-image"]}`}>
+                <Image src={imgUrl} alt="log image"></Image>
+            </div>
+            <div className={`${styles["gpt3__blog-container_article-content"]}`}>
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
+        </div>
+        </>
+    )
 } 
